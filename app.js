1ab25d3e82698dff0f89502095edd824442f5cfa
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 17:37:35 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 // write code here
+const employees = {
+            john: { salary: 1000, address: { district: 'Ratchathewi', province: 'Bangkok' } },
+            peter: { salary: 1500, address: { district: 'Pathumwan', province: 'Bangkok' } },
+            mike: { salary: 800, address: { district: 'Pakkret', province: 'Nonthaburi' } },
+            sarah: { salary: 2200, address: { district: 'Sriraja', province: 'Chonburi' } }
+          };
+          
+          function findEmployee(name) {  
+            name = name.toLowerCase();
+            if (employees.hasOwnProperty(name)) {
+              const employee = employees[name];
+              const message = `Name: ${name}, salary: ${employee.salary}, address: ${employee.address.district}, 
+              ${employee.address.province}`;
+              console.log(message);
+            } else {
+              console.log("Not Found");
+            }
+          }
+          findEmployee("john"); 
+          findEmployee("peter"); 
+          findEmployee("mike"); 
+          findEmployee("sarah"); 
+          findEmployee("mary"); 
+          
+    </script>
